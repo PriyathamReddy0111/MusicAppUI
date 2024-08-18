d9ee761f183cad344fa63ca8edee9d134dd6edb3
Priyatham Reddy <priyathamreddy106@gmail.com>
2024-08-18 11:33:33 +0530

    Final Commit

--- app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -99,7 +99,9 @@ fun MainView() {
                     val tint = if(currentRoute==item.bRoute) Color.White else Color.Black
                     BottomNavigationItem(
                         selected = currentRoute == item.bRoute,
-                        onClick = { controller.navigate(item.bRoute) },
+                        onClick = { controller.navigate(item.bRoute)
+                            title.value = item.bTitle
+                                  },
                         icon = {
                             Icon(
                                 tint = tint,
